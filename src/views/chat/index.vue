c7e713192dbc40795dc403b0acc6e97fff6ac589
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-09 19:44:53 +0000

    Sample

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -433,7 +433,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>пример: Напиши стихотворение о первом снегопаде года в Марте</span>
             </div>
           </template>
           <template v-else>
