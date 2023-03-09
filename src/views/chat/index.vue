9a2a566be601dbb4144d42f5e7614219eafb075c
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-09 16:26:32 +0000

    fixes4

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -432,7 +432,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>What if we lived in a simulation? How would we know, and what implications would this have on our understanding of reality and our place in the universe?</span>
             </div>
           </template>
           <template v-else>
