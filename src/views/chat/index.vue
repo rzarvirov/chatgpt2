69aad0f349d95e40e2c2b37a558904b020873c1b
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-09 19:52:51 +0000

    Sample4

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -433,7 +433,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>пример: придумай список из 10 абсурдных изобретений, которые сделали бы жизнь проще</span>
+              <span>пример: придумай новый праздник и опишите, как он отмечается</span>
             </div>
           </template>
           <template v-else>
