3949215fd9199d3ce587ba6fff210ab1ee17a6bd
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-10 21:52:56 +0000

    authorization3

--- src/store/modules/chat/index.ts
+++ src/store/modules/chat/index.ts
@@ -3,6 +3,53 @@ import { getLocalState, setLocalState } from './helper'
 import { router } from '@/router'
 import { fetchClearChat, fetchCreateChatRoom, fetchDeleteChat, fetchDeleteChatRoom, fetchGetChatHistory, fetchGetChatRooms, fetchRenameChatRoom } from '@/api'
 
+declare namespace Chat {
+
+	interface Chat {
+		uuid?: number
+		dateTime: string
+		text: string
+		inversion?: boolean
+		error?: boolean
+		loading?: boolean
+		conversationOptions?: ConversationRequest | null
+		requestOptions: { prompt: string; options?: ConversationRequest | null }
+	}
+
+	interface History {
+		title: string
+		isEdit: boolean
+		uuid: number
+	}
+
+	interface ChatState {
+		active: number | null
+		history: History[]
+		chat: { uuid: number; data: Chat[] }[]
+	}
+
+	interface ConversationRequest {
+		conversationId?: string
+		parentMessageId?: string
+	}
+
+	interface ConversationResponse {
+		conversationId: string
+		detail: {
+			choices: { finish_reason: string; index: number; logprobs: any; text: string }[]
+			created: number
+			id: string
+			model: string
+			object: string
+			usage: { completion_tokens: number; prompt_tokens: number; total_tokens: number }
+		}
+		id: string
+		parentMessageId: string
+		role: string
+		text: string
+	}
+}
+
 export const useChatStore = defineStore('chat-store', {
   state: (): Chat.ChatState => getLocalState(),
 
