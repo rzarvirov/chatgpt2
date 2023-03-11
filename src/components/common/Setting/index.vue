694c5adb933d5df029fbf57fb96cd776b0e97a98
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-11 22:38:18 +0000

    visual

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -30,7 +30,7 @@ const show = computed({
 </script>
 
 <template>
-  <NModal v-model:show="show" :auto-focus="false">
+  <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
     <div>
       <NTabs v-model:value="active" type="line" animated>
         <NTabPane name="General" tab="General">
