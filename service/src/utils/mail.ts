d5ee49c1468c4dfcb6ae26d5675e11c702a68fa1
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-11 16:51:51 +0000

    email

--- service/src/utils/mail.ts
+++ service/src/utils/mail.ts
@@ -15,9 +15,9 @@ export function sendMail(toMail: string, verifyUrl: string) {
   const mailOptions = {
     from: process.env.SMTP_USERNAME, // sender address
     to: toMail, // list of receivers
-    subject: '账号验证', // Subject line
+    subject: 'aibuddy.ru: Подтверждение регистрации', // Subject line
     // text: 'Hello world?', // plain text body
-    html: `<h1>你正在注册网站，你的邮箱验证链接为（12小时内有效）</h1><br/><br/><a target="_blank" href="${verifyUrl}">点我验证</a>`, // html body
+    html: `<h1>Вы регистрируетесь на сайте aibuddy.ru, ссылка для подтверждения вашего электронного адреса: (действительна в течение 12 часов)</h1><br/><br/><a target="_blank" href="${verifyUrl}">Подтвердить регистрацию</a>`, // html body
   }
 
   // send mail with defined transport object
