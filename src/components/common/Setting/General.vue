f99db02f17b7968b3e5e1c6403401d21e6703ea6
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-12 22:32:45 +0000

    samples10

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -51,8 +51,8 @@ const themeOptions: { label: string; key: Theme; icon: string }[] = [
 ]
 
 const languageOptions: { label: string; key: Language; value: Language }[] = [
-  { label: '简体中文', key: 'zh-CN', value: 'zh-CN' },
-  { label: '繁體中文', key: 'zh-TW', value: 'zh-TW' },
+//  { label: '简体中文', key: 'zh-CN', value: 'zh-CN' },
+//  { label: '繁體中文', key: 'zh-TW', value: 'zh-TW' },
   { label: 'English', key: 'en-US', value: 'en-US' },
   { label: 'Русский', key: 'ru-RU', value: 'ru-RU' },
 ]
