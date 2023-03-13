0a29cd3ae2874545c0330871c4f300069d820455
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-13 16:39:45 +0000

    sample into auth screen refresh function - animated13

--- src/views/chat/layout/Permission.vue
+++ src/views/chat/layout/Permission.vue
@@ -141,7 +141,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <NModal :show="visible" style="width: 90%; max-width: 640px">
+  <NModal :show="visible" style="width: 90%; max-width: 640px; height: 90%;">
     <div class="p-10 bg-white rounded dark:bg-slate-800">
       <div class="space-y-4">
         <header class="space-y-2">
