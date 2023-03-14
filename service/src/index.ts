04f79a388a9b56eda3d4693788df34f203ec3e8c
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-14 21:54:34 +0000

    LoginUpdate7

--- service/src/index.ts
+++ service/src/index.ts
@@ -176,7 +176,7 @@ router.post('/user-register', async (req, res) => {
   const { username, password } = req.body as { username: string; password: string }
 
   if (process.env.REGISTER_ENABLED !== 'true') {
-    res.send({ status: 'Fail', message: '注册账号功能未启用 | Register account is disabled!', data: null })
+    res.send({ status: 'Fail', message: 'Регистрация новых пользователь временно приостановлена', data: null })
     return
   }
   if (typeof process.env.REGISTER_MAILS === 'string' && process.env.REGISTER_MAILS.length > 0) {
@@ -189,25 +189,25 @@ router.post('/user-register', async (req, res) => {
         break
     }
     if (!allowSuffix) {
-      res.send({ status: 'Fail', message: '该邮箱后缀不支持 | The email service provider is not allowed', data: null })
+      res.send({ status: 'Fail', message: 'Такая почтовый сервис не поддерживается', data: null })
       return
     }
   }
 
   const user = await getUser(username)
   if (user != null) {
-    res.send({ status: 'Fail', message: '邮箱已存在 | The email exists', data: null })
+    res.send({ status: 'Fail', message: 'Имя уже используется', data: null })
     return
   }
   const newPassword = md5(password)
   await createUser(username, newPassword)
 
   if (username.toLowerCase() === process.env.ROOT_USER) {
-    res.send({ status: 'Success', message: '注册成功 | Register success', data: null })
+    res.send({ status: 'Success', message: 'Ура!', data: null })
   }
   else {
     sendMail(username, getUserVerifyUrl(username))
-    res.send({ status: 'Success', message: '注册成功, 去邮箱中验证吧 | Registration is successful, you need to go to email verification', data: null })
+    res.send({ status: 'Success', message: 'Проверьте почту для подтверждения регистрации', data: null })
   }
 })
 
@@ -237,19 +237,19 @@ router.post('/user-login', async (req, res) => {
   try {
     const { username, password } = req.body as { username: string; password: string }
     if (!username || !password)
-      throw new Error('用户名或密码为空 | Username or password is empty')
+      throw new Error('Имя или пароль не введены')
 
     const user = await getUser(username)
     if (user == null
       || user.status !== Status.Normal
       || user.password !== md5(password)) {
       if (user != null && user.status === Status.PreVerify)
-        throw new Error('请去邮箱中验证 | Please verify in the mailbox')
-      throw new Error('用户不存在或密码错误 | User does not exist or incorrect password.')
+        throw new Error('Проверьте почту для подтверждения регистрации')
+      throw new Error('Имя не найдено или пароль введен не верно, попробуйте еще')
     }
 
     const token = jwt.sign({ email: username, userId: user._id }, process.env.AUTH_SECRET_KEY)
-    res.send({ status: 'Success', message: '登录成功 | Login successfully', data: { token } })
+    res.send({ status: 'Success', message: 'Добро пожаловать!', data: { token } })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
@@ -263,7 +263,7 @@ router.post('/verify', async (req, res) => {
       throw new Error('Secret key is empty')
     const username = await checkUserVerify(token)
     await verifyUser(username)
-    res.send({ status: 'Success', message: '验证成功 | Verify successfully', data: null })
+    res.send({ status: 'Success', message: 'Добро пожаловать!', data: null })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
