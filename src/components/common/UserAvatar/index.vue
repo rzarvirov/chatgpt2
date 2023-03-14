150d446e9dbce493e0b0fa8f75a783ac3a93035d
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-14 23:42:00 +0000

    LoginUpdate14

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -1,21 +1,13 @@
 <script setup lang='ts'>
-import { computed, onMounted, ref } from 'vue'
+import { computed } from 'vue'
 import { NAvatar } from 'naive-ui'
 import { useUserStore } from '@/store'
 import defaultAvatar from '@/assets/avatar.png'
 import { isString } from '@/utils/is'
-import { getUserBalance } from '@/../service/src/storage/mongo' // Import the getUserBalance function, replace the path with the correct path to your mongo.ts file
 
 const userStore = useUserStore()
 
 const userInfo = computed(() => userStore.userInfo)
-
-const userBalance = ref(0) // Declare a ref to store the user's balance
-
-onMounted(async () => {
-  if (userInfo.value._id)
-    userBalance.value = await getUserBalance(userInfo.value._id) // Fetch the user's balance and store it in the userBalance ref
-})
 </script>
 
 <template>
