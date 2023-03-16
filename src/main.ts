05d147e194d0c7ca5cc153391071efe629d771bd
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-16 20:41:49 +0000

    headers3

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { register } from 'swiper/element/bundle'
 import App from './App.vue'
 import { setupI18n } from './locales'
-import { setupAssets } from './plugins'
+import { setupAssets, setupScrollbarStyle } from './plugins'
 import { setupStore } from './store'
 import { setupRouter } from './router'
 import 'swiper/swiper-bundle.min.css'
@@ -11,6 +11,8 @@ register()
 async function bootstrap() {
   const app = createApp(App)
   setupAssets()
+
+  setupScrollbarStyle()
 
   setupStore(app)
 
