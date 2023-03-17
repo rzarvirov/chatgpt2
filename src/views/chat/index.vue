bcd889cef205e6d8801fcd22e11695803f5b8df0
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-17 19:44:26 +0000

    gpt4.2

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -588,7 +588,7 @@ const getColourForKey = (key: string) => {
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:service-fill" class="mr-2 text-3xl" />
               <span>
-                <b><u><a href="https://pay.cloudtips.ru/p/99817dfa" target="_blank">Поддержать проект</a></u></b>
+                Модель: gpt-4 (OpenAI) | <b><u><a href="https://pay.cloudtips.ru/p/99817dfa" target="_blank">Поддержать проект</a></u></b>
               </span>
             </div>
             <br>
