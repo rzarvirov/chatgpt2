10a170e9f99b098b0cbec047e1d9d79b9ecbf372
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-18 17:26:47 +0000

    rollback2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { register } from 'swiper/element/bundle'
-import GAuth from 'vue-google-oauth2'
 import App from './App.vue'
 import { setupI18n } from './locales'
 import { setupAssets, setupScrollbarStyle } from './plugins'
@@ -9,14 +8,6 @@ import { setupRouter } from './router'
 import 'swiper/swiper-bundle.min.css'
 
 register()
-
-const gAuthOptions = {
-  clientId: '474493346119-5o0f10gmqbr1ecdj8is1igk74jp65422.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account',
-}
-
-App.use(GAuth, gAuthOptions)
 
 async function bootstrap() {
   const app = createApp(App)
