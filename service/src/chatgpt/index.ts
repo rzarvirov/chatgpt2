b9c3b22caa111c98422dbed69ef22327b5e852a7
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-19 23:15:11 +0000

    timeout from 1000 to 10000

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -21,7 +21,7 @@ const ErrorCodeMessage: Record<string, string> = {
 
 dotenv.config()
 
-const timeoutMs: number = !isNaN(+process.env.TIMEOUT_MS) ? +process.env.TIMEOUT_MS : 30 * 1000
+const timeoutMs: number = !isNaN(+process.env.TIMEOUT_MS) ? +process.env.TIMEOUT_MS : 30 * 10000
 
 let apiModel: ApiModel
 
